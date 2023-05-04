7db0e7739b6daa83692b14e5fd3aec0626b0b619
Francisc088 <fran_araujo_01@outlook.com>
2023-05-04 04:26:56 -0300

    primer commit backend

---
+++ src/test/java/com/portfolio/afe/AfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.afe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
